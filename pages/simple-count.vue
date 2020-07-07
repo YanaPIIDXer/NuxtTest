a8fd4ab94f3d6200e31020302fe66e9277b3751e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-07 10:31:08 +0900

    単純にカウントアップするだけの実験用ページを作成。

---
+++ pages/simple-count.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        単純カウント
+      </h1>
+      <nuxt-link to="/">トップページ</nuxt-link><br />
+    </div>
+  </div>
+</template>
+
+<script>
+export default
+{
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
